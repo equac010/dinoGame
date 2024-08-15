0ba98dd50c1a4b59aa39366088a38ee7c227f493
Ethan <equac010@ucr.edu>
2024-08-15 01:01:56 -0700

    First commit for game code. Added working sprite and ground

---
+++ mygame/src/main.js
@@ -0,0 +1,37 @@
+import kaplay from "kaplay";
+import "kaplay/global";
+
+const k = kaplay({
+	debug: true,
+	background: [135, 206, 235,],
+});
+
+
+k.loadSprite("bunny", "sprites/bunny.png")
+k.setGravity(1600);
+
+
+
+const bunny = k.add([
+	k.pos(80, 40),
+	k.sprite("bunny"),
+	k.body(),
+	k.area(0.8),
+])
+
+onKeyPress("space", () => {
+    if (bunny.isGrounded()) {
+        bunny.jump();
+    }
+});
+
+const ground = k.add([
+    rect(width(), 48),
+    pos(0, height() - 48),
+    outline(4),
+    area(),
+    body({ isStatic: true }),
+    color(255, 255, 255),
+]);
+
+k.onClick(() => k.addKaboom(k.mousePos()))
